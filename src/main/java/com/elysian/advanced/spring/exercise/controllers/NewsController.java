557daa7e2761bd642d80bba13e55f9a76dfe9511
fcodau <Fabian.Codau.extern@ssi-schaefer.com>
2022-04-11 00:53:36 +0300

    Add controllers

---
+++ src/main/java/com/elysian/advanced/spring/exercise/controllers/NewsController.java
@@ -0,0 +1,49 @@
+package com.elysian.advanced.spring.exercise.controllers;
+
+import com.elysian.advanced.spring.exercise.models.News;
+import com.elysian.advanced.spring.exercise.services.NewsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping(
+        path = "/news"
+)
+public class NewsController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NewsController.class);
+
+    private final NewsService newsService;
+
+    @Autowired
+    public NewsController(NewsService newsService) {
+        this.newsService = newsService;
+    }
+
+    @PostMapping("/createNews")
+    public ResponseEntity<?> createNews(@RequestBody News news) {
+        newsService.createNews(news);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @GetMapping("/getNewsByAuthor/{author}")
+    public List<News> getNewsByAuthor(@PathVariable final String author) {
+        LOGGER.info("Start time: {}", new Date(System.currentTimeMillis()));
+        List<News> news = newsService.getNewsByAuthor(author);
+        LOGGER.info("Stop time: {}", new Date(System.currentTimeMillis()));
+        return news;
+    }
+
+}
---
+++ src/main/java/com/elysian/advanced/spring/exercise/controllers/security/UserController.java
@@ -0,0 +1,70 @@
+package com.elysian.advanced.spring.exercise.controllers.security;
+
+import com.elysian.advanced.spring.exercise.models.security.Role;
+import com.elysian.advanced.spring.exercise.models.security.User;
+import com.elysian.advanced.spring.exercise.services.security.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addToUser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm roleToUser) {
+        userService.addRoleToUser(roleToUser.getUsername(), roleToUser.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+    class RoleToUserForm {
+        private String username;
+        private String roleName;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getRoleName() {
+            return roleName;
+        }
+
+        public void setRoleName(String roleName) {
+            this.roleName = roleName;
+        }
+    }
+}
